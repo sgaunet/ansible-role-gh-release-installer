6f91d520cda6fd7742d214bdc803e6a2e801ba6f
Sylvain Gaunet <1552102+sgaunet@users.noreply.github.com>
2024-06-27 20:46:15 +0200

    chore: Exclude 'main' YAML files from default generation

--- gen-ci.sh
+++ gen-ci.sh
@@ -8,7 +8,7 @@ if [ ! -d docs ]; then
   exit 1
 fi
 
-find defaults -name '*.yml' | sort | while read -r doc
+find defaults -name '*.yml' | sort | grep -v main | while read -r doc
 do
   f=$(basename "$doc")
   bin_name=$(echo "$f" | cut -d'.' -f1)
